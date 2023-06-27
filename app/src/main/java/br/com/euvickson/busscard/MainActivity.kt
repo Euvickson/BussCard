dc03b35d14828a08f1627d2a5296ba74a53c5105
Euvickson <victor.eugennio@gmail.com>
2023-06-27 20:29:28 -0300

    Creating the Content, and the logic to show the content when the button state is equal to true

--- app/src/main/java/br/com/euvickson/busscard/MainActivity.kt
+++ app/src/main/java/br/com/euvickson/busscard/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,6 +27,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -54,6 +58,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBusCard() {
+    val buttonClickedState = remember { mutableStateOf(false) }
     Surface(modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
@@ -76,13 +81,50 @@ fun CreateBusCard() {
                 CreateInfo()
 
                 Button(
-                    onClick = {}) {
+                    onClick = {
+                        buttonClickedState.value = !buttonClickedState.value
+                    }) {
                     Text(
                         text = "Portfolio",
                         style = MaterialTheme.typography.titleMedium
                     )
                 }
+                if (buttonClickedState.value) {
+                    Content()
+                } else {
+                    Box() {}
+                }
             }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Content() {
+
+    Box(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .padding(5.dp)) {
+        Surface(modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            Portfolio(data = listOf("Project 1", "Project 2", "Project 3"))
+        }
+    }
+
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn() {
+        items (data.size) { index ->
+            Text(data[index])
         }
     }
 }
@@ -121,7 +163,7 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BussCardTheme {
